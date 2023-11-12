ed48506518916952394b05d8181a32744e148bc1
Juho <jeuslee99@naver.com>
2023-11-12 18:00:56 +0900

    connect database

--- Accounts/models.py
+++ Accounts/models.py
@@ -2,3 +2,42 @@ from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, password, **kwargs):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email=None, nickname=None, password=None):
+        superuser = self.create_user(
+            email=email,
+            nickname=nickname,
+            password=password,
+        )
+        superuser.is_staff = True
+        superuser.is_superuser = True
+        superuser.is_active = True
+        superuser.save(using=self._db)
+        return superuser
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    email = models.EmailField(max_length=100, blank=False, null=False, unique=True)
+    nickname = models.CharField(max_length=20, blank=False, null=False, unique=True, default=generate_random_nickname)
+
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname']
--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+#db 연동
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,8 +87,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lkk',
+        'USER' : 'root',
+        'PASSWORD' : 'apolonlee9',
+        'HOST': '',
+        'PORT': '3306'
     }
 }
 
