076a61094763f251dd4a596a23f3cf295c0a6a7d
Juho <jeuslee99@naver.com>
2023-11-22 03:48:02 +0900

    기자재 삭제 API 개발 및 예외 처리

--- Equipments/views.py
+++ Equipments/views.py
@@ -1,6 +1,4 @@
 from rest_framework.exceptions import ValidationError
-from django.db import IntegrityError
-
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import Http404
@@ -36,7 +34,7 @@ class InventoryAPIView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except KeyError:
             return Response({"message": "model_name을 올바르게 전달하세요."}, status=status.HTTP_400_BAD_REQUEST)
-        
+
 
     #기자재 정보 수정 API
     @login_check
@@ -59,7 +57,6 @@ class InventoryAPIView(APIView):
             return Response({"message": "model_name을 올바르게 전달하세요."}, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class InventoryDetailAPIView(APIView):
     # 기자재 정보 조회 API
     def get(self, request, pk):
@@ -67,10 +64,21 @@ class InventoryDetailAPIView(APIView):
             equip = Equipment.objects.get(pk=pk)
             serializer = EquipmentSerializer(equip)
 
-            return Response({"message": "수정 완료" + serializer.data}, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Equipment.DoesNotExist:
-            failMessage = {
-                "message": "해당 기자재의 정보가 존재하지 않습니다."
-            }
-            return Response(failMessage, status=status.HTTP_404_NOT_FOUND)
+            return Response({"message": "해당 기자재의 정보가 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
 
+    #기자재 정보 삭제 API
+    @login_check
+    def delete(self, request, pk):
+        try:
+            if request.user.is_staff:
+                equip = get_object_or_404(Equipment, pk= pk)
+
+                # 삭제 진행
+                equip.delete()
+                return Response({"message": "삭제 성공"}, status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({"message": "기자재를 삭제할 권한이 없습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+        except Http404:
+            return Response({"message": "삭제할 기자재를 찾지 못했습니다."}, status=status.HTTP_404_NOT_FOUND)
