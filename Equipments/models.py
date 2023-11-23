c34fb0be30d9d51f1d4d578d39029af62792a6e7
Juho <jeuslee99@naver.com>
2023-11-24 07:19:40 +0900

    기자재 반납-대여를 위한 모델 만들기

--- Equipments/models.py
+++ Equipments/models.py
@@ -24,15 +24,15 @@ class Equipment(models.Model):
         db_table= 'equipment'
 
 class Log(models.Model):
-    #log_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey('Accounts.User', on_delete=models.CASCADE,db_column='u_id' , related_name='log_user_id')
     model_name = models.ForeignKey('Equipment', on_delete=models.CASCADE, db_column='model_name')
     rent_count= models.IntegerField(default=0)
     return_deadline = models.DateTimeField(blank= True, null= True, default= None)
     rent_requested_date = models.DateTimeField(blank= True, null= True, default= None)
-    rent_accepted_date = models.DateTimeField(blank= True, null= True, default= None)
+    rent_accepted_date = models.DateTimeField(blank= True, null= True, default= None, unique=True)
     return_requested_date = models.DateTimeField(blank= True, null= True, default= None)
-    return_accepted_date = models.DateTimeField(blank= True, null= True, default= None)
+    return_accepted_date = models.DateTimeField(blank= True, null= True, default= None, unique=True)
     rent_price= models.IntegerField(blank=True, null=True, default= None)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -40,5 +40,33 @@ class Log(models.Model):
         #managed= False
         db_table = 'log'
 
-# class Renting(models.Model):
-#     log_id = models.ForeignKey('Equipnents.Log')
+class Renting(models.Model):
+    log_id = models.ForeignKey(Log, on_delete=models.CASCADE, db_column='log_id', related_name='renting_log_id', null=True)
+    rent_accepted_date = models.ForeignKey(Log, on_delete=models.CASCADE, db_column='rent_accepted_date', to_field='rent_accepted_date',
+                                            null= True)
+    user_id = models.ForeignKey('Accounts.User', on_delete=models.CASCADE, db_column='u_id', related_name= 'renting_user_id')
+
+    class Meta:
+        #managed= False
+        db_table= 'renting'
+
+
+class Returned(models.Model):
+    log_id = models.ForeignKey(Log, on_delete=models.CASCADE, db_column='log_id', related_name='returned_log_id',
+                               null=True)
+    return_accepted_date = models.ForeignKey(Log, on_delete=models.CASCADE, db_column='return_accepted_date',
+                                           to_field='return_accepted_date',
+                                           null=True)
+    class Meta:
+        #managed=False
+        db_table= 'returned'
+
+class Returning(models.Model):
+    log_id = models.ForeignKey(Log, on_delete=models.CASCADE, db_column='log_id', related_name='returning_log_id',
+                               null=True)
+    user_id = models.ForeignKey('Accounts.User', on_delete=models.CASCADE, db_column='u_id',
+                                related_name='returning_user_id')
+
+    class Meta:
+        #managed=False
+        db_table= 'returning'
